be41638c86e1f392d21fa8814d5480afd0b36532
Jorge López Puebla <jlpenero2005@gmail.com>
2024-09-12 21:52:35 +0200

    Done for today

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,14 @@
 import { parseURL } from "./utilities.js";
 import { loadComponent } from "./components.js";
 
-const content = document.getElementById("content");
+// TODO (Handle Routing)
+function handleRouting() {
+  const path = window.location.pathname;
+
+  if (path.startsWith("/app/")) {
+    window.history.replaceState("", "", path);
+  }
+}
 
 if (!localStorage.getItem("lang")) {
   localStorage.setItem("lang", "en"); // TODO (Based on browser / system preferences, IP or location)
@@ -12,10 +19,22 @@ let path = parseURL();
 window.page = path[1] || "home";
 loadComponent();
 
+// Sidebar navigation
+document.querySelector(".sidebar").addEventListener("click", function (event) {
+  let url = event.target.closest("a")?.id;
+  if (url) window.history.replaceState("", "", url);
+});
+
 // Detect URL change and load the matching component
 window.addEventListener("popstate", () => {
+  console.log("Hi");
   if (parseURL()[1] != window.page) {
     window.page = path[1] || "home";
     loadComponent();
   }
 });
+
+// On-screen notification function
+function notify(message) {
+  // TODO (Notification)
+}
