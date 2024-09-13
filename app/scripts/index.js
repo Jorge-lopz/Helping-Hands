7c316de4c07b6e10f7c9a6c288e5a3fa112b2b23
Jorge López Puebla <jlpenero2005@gmail.com>
2024-09-13 18:41:54 +0200

    Added title changing when switching components

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,39 +1,31 @@
-import { parseURL } from "./utilities.js";
-import { loadComponent } from "./components.js";
+import { changePage } from "./utilities.js";
 
 if (!localStorage.getItem("lang")) {
   localStorage.setItem("lang", "en"); // TODO (Based on browser / system preferences, IP or location)
 }
 window.lang = localStorage.getItem("lang");
 
-let path = parseURL();
-window.page = path[1] || "home";
-loadComponent();
+changePage();
+addEventListener("pageChange", () => changePage());
 
 // Sidebar navigation
 document.querySelector(".sidebar").addEventListener("click", function (event) {
   let url = event.target.closest("a")?.id;
-  if (url) window.history.replaceState("", "", url.replace(/^home$/, "/app"));
+  if (url) window.history.replaceState("", "", url.replace(/^home$/, "/app/"));
 });
 
-// Override the replaceState method (Adding custom event)
-(function () {
-  const originalReplaceState = window.history.replaceState;
-  window.history.replaceState = function (...args) {
-    originalReplaceState.apply(window.history, args);
-    // Create and dispatch a custom event
-    window.dispatchEvent(new Event("pageChange"));
-  };
-})();
-
-// Detect URL change and load the matching component
-window.addEventListener("pageChange", () => {
-  path = parseURL();
-  if (path[1] != window.page) {
-    window.page = path[1] || "home";
-    loadComponent();
+// Override the replaceState and pushState method (Adding custom event dispatching)
+function addEventDispatching(target, method, event) {
+  const originalMethod = target[method];
+  function newMethod(...args) {
+    originalMethod.apply(target, args);
+    window.dispatchEvent(new Event(event));
   }
-});
+  target[method] = newMethod;
+}
+
+addEventDispatching(window.history, "replaceState", "pageChange");
+addEventDispatching(window.history, "pushState", "pageChange");
 
 // On-screen notification function
 function notify(message) {
--- app/scripts/utilities.js
+++ app/scripts/utilities.js
@@ -1,8 +1,25 @@
 import routes from "./routes.js";
+import { loadComponent } from "./components.js";
+
+Object.defineProperty(String.prototype, "capitalize", {
+  value: function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  },
+  enumerable: false,
+});
+
+// TODO (Create checks for each URL)
+// Checks the URL and returns the highest valid URL (Removing everything after the first mistake)
+function validURL(pathSegments) {
+  // SEARCH -> Allows 'users/', 'offers/' and 'requests/', the actual user input appears as a param (?q=input)
+  // HOME -> Doesn't allow any further URLs
+  // FRIENDS -> Doesn't allow any further URLs
+}
 
 // Function to parse the URL for the page and the extra parameters
 function parseURL() {
   const pathSegments = window.location.pathname.split("/").filter((segment) => segment.trim() !== "");
+  /* TODO Check if valid */
   if (pathSegments[1] && !routes[pathSegments[1]]) {
     window.history.replaceState("", "", "/app/");
     return ["app", "home"];
@@ -10,4 +27,13 @@ function parseURL() {
   return pathSegments;
 }
 
-export { parseURL };
+function changePage() {
+  let path = parseURL();
+  if (path[1] != window.page) {
+    window.page = path[1] || "home";
+    document.getElementsByTagName("title")[0].innerHTML = `Helping Hands | ${window.page.capitalize()}`;
+    loadComponent();
+  }
+}
+
+export { parseURL, changePage };
